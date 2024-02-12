50a8047c022f572352dda7c60df07ada602ed3a9
ashishpanchal5494 <ashishpanchal199@gmail.com>
2024-02-12 12:35:54 +0530

    first commit

---
+++ lib/validations/user.ts
@@ -0,0 +1,17 @@
+import * as z from "zod";
+
+export const UserValidation = z.object({
+  profile_photo: z.string().url().nonempty(),  // Corrected typo
+  name: z
+    .string()
+    .min(3, { message: "Name must be at least 3 characters." })
+    .max(30, { message: "Name must be at most 30 characters." }),
+  username: z
+    .string()
+    .min(3, { message: "Username must be at least 3 characters." })
+    .max(30, { message: "Username must be at most 30 characters." }),
+  bio: z
+    .string()
+    .min(3, { message: "Bio must be at least 3 characters." })
+    .max(1000, { message: "Bio must be at most 1000 characters." }),
+});
